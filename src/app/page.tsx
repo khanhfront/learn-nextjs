021d26dd5183ac7e389a58b8922a6d1bbb6968c9
khanhfront <kkhanh258@gmail.com>
2024-08-03 13:24:57 +0700

    starter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
+          Made by KaneNoPain&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
         </p>
         <div>
@@ -28,16 +28,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <div className={styles.center}>Kane is here</div>
 
       <div className={styles.grid}>
         <a
